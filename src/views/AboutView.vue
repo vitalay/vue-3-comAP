bf43b8873de445b8d3b28fcd476f5ef5b77f1460
vitalya <vitalay.efimov@gmail.com>
2024-12-29 22:09:02 +0500

    pinia getter

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="About">
     <h1>About</h1>
+    <button class="counter-button"
+    @click="spore.plusCnopka"
+    >
+      {{ spore.count }}
+    </button>
   </div>
 </template>
+<script setup>
+import { userCounterStore } from '@/stores/counter'
 
+const spore = userCounterStore();
+
+</script>
+
+<style>
+.counter-button {
+  font-size: 50px;
+}
+</style>
