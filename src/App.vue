9fc751486676b3b8588b77c2675d5c788c2a2a68
vitalya <vitalay.efimov@gmail.com>
2024-12-29 21:09:17 +0500

    счетчик

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div class="app">
+      <nav>
+        <RouterLink to="/">Home</RouterLink> | 
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </div>
+
+  <RouterView />
+</template>
+<style>
+ .app {
+  text-align: center;
+ }
+</style>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+
+<template>
+  <div class="Home">
+   <div class="count">
+    {{ count }}
+   </div>
+   <div class="button">
+    <button @click="minys">-</button>
+    <button @click="plus">+</button>
+   </div>
+   <br>
+   <div>
+    This counter is: {{ oddOrEven }}
+   </div>
+  
+  </div>
+
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+
+const  count = ref(0)
+
+const plus = () => {
+  count.value++
+}
+const minys = () => {
+  count.value--
+}
+const oddOrEven = computed(() => {
+  if (count.value % 2 === 0 ) return 'even'
+  return 'odd'
+})
+
+</script>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+.buttons {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
